f86c3eff52811bc2eff786e2b95a940fa2165159
Sébastien Counen <sebastiencounen@Aureliens-MBP.lan>
2021-02-04 10:13:06 +0100

    Fix dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
 
+import createAction from './utils/createAction.utils';
+
 import { INCREMENT, DECREMENT } from './actions/counter.actionTypes';
 import { SIGN_IN, SIGN_OUT } from './actions/auth.actionTypes';
 
-function App() {
+const App = () => {
   const counter = useSelector((state: RootStateOrAny) => state.counter);
   const isLogged = useSelector((state: RootStateOrAny) => state.auth);
 
@@ -13,14 +15,20 @@ function App() {
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(INCREMENT)}>+</button>
-      <button onClick={() => dispatch(DECREMENT)}>-</button>
+      <div>
+        <button onClick={() => dispatch(createAction(INCREMENT))}>+</button>
+        <button onClick={() => dispatch(createAction(DECREMENT))}>-</button>
+      </div>
 
       {isLogged && <h3>Information I shouldn't see if I'm not logged in</h3>}
-      <button onClick={() => dispatch(SIGN_IN)}>Log in</button>
-      <button onClick={() => dispatch(SIGN_OUT)}>Log out</button>
+      <div>
+        <button onClick={() => dispatch(createAction(SIGN_IN))}>Log in</button>
+        <button onClick={() => dispatch(createAction(SIGN_OUT))}>
+          Log out
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
